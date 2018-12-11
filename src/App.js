d579a86ac5571863e4d1d77bac635c973aa430c2
Hakan Akgunes <hakanakgunes87@gmail.com>
2018-12-11 22:50:44 +0300

    last change for app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            New Edit <code>src/App.js</code> and save deneme to reload.
+            Last Edit <code>src/App.js</code> and save deneme to reload.
           </p>
           <a
             className="App-link"
